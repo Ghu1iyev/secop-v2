afad1306b749baa8eba215c6ded2d9adf4e43e11
perinov <novruzovaperi2@gmail.com>
2025-05-24 03:53:42 +0400

    added layout

---
+++ src/layout/Header/index.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { LogoIcon } from "../../../public/assets/images/vector";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="container pt-16 flex justify-between items-center">
+      <div>
+        <Link href={"/"}>
+          <LogoIcon />
+        </Link>
+      </div>
+
+      <div className="flex justify-between items-center gap-[136px]">
+        <nav>
+          <ul className="flex gap-14 text-xl font-semibold text-[#B0B0B099]">
+            <li>
+              <Link href={"about"}>About Us</Link>
+            </li>
+            <li>
+              <Link href={"projects"}>Projects</Link>
+            </li>
+            <li>
+              <Link href={"services"}>Services</Link>
+            </li>
+            <li>
+              <Link href={"certificates"}>Certificates</Link>
+            </li>
+            <li>
+              <Link href={"team"}>Team</Link>
+            </li>
+            <li>
+              <Link href={"contacts"}>Contacts</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div>
+          <Select>
+            <SelectTrigger className="w-[121px] rounded-xl bg-[#121212CC] border-[#2A2A2A] text-[#B0B0B0] font-semibold text-xl ">
+              <SelectValue placeholder="ENG" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {/* <SelectLabel>ENG</SelectLabel> */}
+                <SelectItem value="aze">AZE</SelectItem>
+                <SelectItem value="eng">ENG</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+    </header>
+  );
+}
