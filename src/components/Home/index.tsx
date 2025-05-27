cf8a3737c05d01b3e97584aabb4a1f845f72e889
Fazil <guliyevfazil5@gmail.com>
2025-05-27 22:34:46 +0400

    updated

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import AboutUs from './about-us'
-import PartnersSlider from './partners-slider'
 import OurProjects from './our-projects'
 import Services from './home-services'
 import Marquee from './marquee'
 import Certificates from './certificates'
 import Blogs from './blogs'
 import CompanySwiper from '../swiper/company-swiper'
+import PartnersSlider from '../swiper/partners-slider'
 
 
 const Home = () => {
