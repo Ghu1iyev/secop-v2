b6da20a8388e378f44cbafa46169449e8365c2cd
Fazil <guliyevfazil5@gmail.com>
2025-05-28 14:25:06 +0400

    service page added

--- src/components/shared/Title/Title.tsx
+++ src/components/shared/Title/Title.tsx
@@ -3,13 +3,14 @@ import React from "react";
 type SectionHeaderProps = {
   title: string;
   subtitle?: string;
+  fontSize?: string
 };
 
-const Title = ({ title, subtitle }: SectionHeaderProps) => {
+const Title = ({ title, subtitle, fontSize }: SectionHeaderProps) => {
   return (
     <div className="flex flex-col gap-6 mt-20">
       <h4 className="text-xl font-normal leading-3 text-[#B0B0B0]">{title} </h4>
-      <p className="text-[36px] font-vesber leading-10 text-[#fff]">{subtitle} </p>
+      <p className={`text-[36px] font-vesber leading-10 text-[#fff] ${fontSize}`}>{subtitle} </p>
     </div>
   );
 };
