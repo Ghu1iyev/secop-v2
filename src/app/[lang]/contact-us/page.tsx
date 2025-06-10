e015dd6098124426495ed72ad4df7f7004201132
Fazil <guliyevfazil5@gmail.com>
2025-06-10 20:49:03 +0400

    contact added

--- src/app/[lang]/contact-us/page.tsx
+++ src/app/[lang]/contact-us/page.tsx
@@ -1,24 +1,63 @@
 "use client";
 import Title from "@/components/shared/Title/Title";
-import { TextInput } from "@mantine/core";
+import { Input, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import React from "react";
 import { IoMdArrowForward } from "react-icons/io";
 import ContactInfo from "./contact-info";
 import TitleAnimation from "@/components/title-animation";
+import { useMutation } from "@tanstack/react-query";
+import { PostApi } from "@/lib/axios";
+import { toast } from "react-toastify";
+import { IMaskInput } from "react-imask";
+
+interface ContactValues {
+  name: string;
+  phone: string;
+  message: string;
+  email: string;
+}
 
 const ContactUs = () => {
   const form = useForm({
     mode: "uncontrolled",
     initialValues: {
+      name: "",
       email: "",
-      termsOfService: false,
+      phone: "",
+      message: "",
     },
 
     validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      email: (value) =>
+        value.trim() === ""
+          ? "Email adresi boş ola bilməz"
+          : /^\S+@\S+$/.test(value)
+          ? null
+          : "Email adresini düzgün daxil edin",
+      name: (value) => (value.trim() === "" ? "Ad boş ola bilməz" : null),
+      phone: (value) =>
+        value.trim() === ""
+          ? "Telefon nömrəsi boş ola bilməz"
+          : !/^\+?[0-9]{7,15}$/.test(value)
+          ? "Telefon nömrəsi düzgün daxil edilməyib"
+          : null,
+      message: (value) => (value.trim() === "" ? "Mesaj boş ola bilməz" : null),
     },
   });
+
+  const mutation = useMutation({
+    mutationKey: ["contact"],
+    mutationFn: async (res: ContactValues) => {
+      return await PostApi("/contact/", res);
+    },
+    onSuccess: () => {
+      toast.success("Mesaj göndərildi");
+    },
+  });
+  const handleSubmit = (values: ContactValues) => {
+    mutation.mutate(values);
+  };
   return (
     <main className="container">
       <div className="mt-20 lg:w-[990px]">
@@ -27,18 +66,18 @@ const ContactUs = () => {
           fontSize="text-[20px] sm:text-[48px]"
           subtitle="Drop us a message — we’ll get back shortly."
         />
-      <p
-        className="
+        <p
+          className="
       text-[#B0B0B0] text-[20px] font-monda leading-[35px] mt-[40px]"
-      >
-        Have a project in mind or need expert advice on cybersecurity? Fill out
-        the form below and let us know how we can help. Our team will get back
-        to you shortly with the information or support you need.
-      </p>
+        >
+          Have a project in mind or need expert advice on cybersecurity? Fill
+          out the form below and let us know how we can help. Our team will get
+          back to you shortly with the information or support you need.
+        </p>
       </div>
       <form
         className="my-[80px] lg:w-[675px]"
-        onSubmit={form.onSubmit((values) => console.log(values))}
+        onSubmit={form.onSubmit(handleSubmit)}
       >
         <TextInput
           size="lg"
@@ -53,26 +92,32 @@ const ContactUs = () => {
           description="Email address"
           className="mb-[40px] form-input"
           placeholder="example@gmail.com"
-          key={form.key("name")}
-          {...form.getInputProps("name")}
+          key={form.key("email")}
+          {...form.getInputProps("email")}
         />
-        <TextInput
-          size="lg"
+        <Input.Wrapper
+          className="mb-[40px] contact-phone-input"
           description="Your number"
-          placeholder="+994"
-          className="mb-[40px] form-input"
-          key={form.key("name")}
-          {...form.getInputProps("name")}
-        />
+          error={form.errors.phone}
+        >
+          <Input
+            size="lg"
+            component={IMaskInput}
+            mask="+994 (55) 000-00-00"
+            placeholder="Your phone"
+            key={form.key("phone")}
+            {...form.getInputProps("phone")}
+          />
+        </Input.Wrapper>
         <TextInput
           size="lg"
           className="mb-[40px] form-input"
           description="Add your message"
           placeholder="Write here your message.."
-          key={form.key("name")}
-          {...form.getInputProps("name")}
+          key={form.key("message")}
+          {...form.getInputProps("message")}
         />
-        <button className="mt-[50px] border border-[#fff] w-full rounded-[12px] text-center  justify-center flex gap-[16px] text-[20px] font-monda py-[12px] sm:py-[20px] items-center text-[#fff]">
+        <button className="mt-[50px] border-solid border border-[#fff] w-full rounded-[12px] text-center  justify-center flex gap-[16px] text-[20px] font-monda py-[12px] sm:py-[20px] items-center text-[#fff]">
           Send your message <IoMdArrowForward />
         </button>
       </form>
--- src/components/article-item/index.tsx
+++ src/components/article-item/index.tsx
@@ -1,23 +1,38 @@
 import Image from "next/image";
 import React from "react";
 
-const ArticleItem = () => {
+interface ArticleData {
+  category: string;
+  name: string;
+  image?: string;
+}
+
+interface ArticleProps {
+  data: ArticleData;
+}
+const ArticleItem = ({ data } : ArticleProps) => {
   return (
     <div className="border-b py-[20px] sm:py-[30px] flex gap-[24px] items-center border-b-[#424242] ">
       <div className="relative flex-none w-[78px] sm:w-[93px]   h-[82px] sm:h-[96px] overflow-hidden rounded-[10px]">
         <Image
-        alt=""
-        fill
-        placeholder="blur"
-        blurDataURL="/assets/images/jpg/8a7324733c75c962d29ca33a935286eebf42afac.jpg"
-        src="/assets/images/jpg/8a7324733c75c962d29ca33a935286eebf42afac.jpg"
-        objectFit="cover"
-      />
+          alt=""
+          fill
+          placeholder="blur"
+          blurDataURL={data?.image || ""}
+          src={data?.image || ""}
+          objectFit="cover"
+        />
       </div>
       <div>
-        <p className="text-[13px] sm:text-[18px] font-monda text-[#fff]">Time to read : 3 min</p>
-        <p className="text-[14px] sm:text-[15px] font-monda text-[#B0B0B0]">Audits & Compliance</p>
-        <h3 className="mt-[18px] sm:mt-[20px] text-[16px] sm:text-[22px] font-vesber sm:overflow-hidden sm:whitespace-nowrap sm:text-ellipsis">The Role of AI in Next-Gen Security Operations</h3>
+        <p className="text-[13px] sm:text-[18px] font-monda text-[#fff]">
+          Time to read : 3 min
+        </p>
+        <p className="text-[14px] sm:text-[15px] font-monda text-[#B0B0B0]">
+          {data?.category}
+        </p>
+        <h3 className="mt-[18px] sm:mt-[20px] text-[16px] sm:text-[22px] font-vesber sm:overflow-hidden sm:whitespace-nowrap sm:text-ellipsis">
+          {data?.name}
+        </h3>
       </div>
     </div>
   );
