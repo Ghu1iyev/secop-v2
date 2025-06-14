b26b74d9623e15389069e8d3f8c9683cb51cd836
Fazil <guliyevfazil5@gmail.com>
2025-06-14 17:53:05 +0400

    build fix

--- src/layout/Header/mobile.tsx
+++ src/layout/Header/mobile.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import { useLanguage } from "@/context/LanguageProvider";
-import { usePathname, useRouter } from "next/navigation";
 import { CancelIcon } from "../../../public/assets/images/vector";
 import { useTranslation } from "@/utils/i18n";
 
