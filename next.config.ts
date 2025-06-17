7f835436e4d365431b1c50e2330cb13a012584a6
Fazil <guliyevfazil5@gmail.com>
2025-06-18 02:10:11 +0400

    update

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{
-      protocol: "https",
-      hostname: "backend.secop.az",
-    }],
+    domains: ["secop.az", "backend.secop.az"], 
   },
 };
 
