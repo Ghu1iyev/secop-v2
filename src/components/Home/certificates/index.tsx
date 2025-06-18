4f0684bce2a9536356f624fcc110aa942c37c37c
Fazil <guliyevfazil5@gmail.com>
2025-06-18 09:29:30 +0400

    about page updated

--- src/components/Home/certificates/index.tsx
+++ src/components/Home/certificates/index.tsx
@@ -2,15 +2,13 @@
 import React from "react";
 import CertificateSwiper from "@/components/swiper/certificates-swiper";
 import TitleAnimation from "@/components/title-animation";
-import { useTranslation } from "@/utils/i18n";
 import { CertificateProps } from "@/types/common";
 
-const Certificates = ({data} : {data: CertificateProps}) => {
-  const { t } = useTranslation();
+const Certificates = ({data, title} : {data: CertificateProps, title?: string}) => {
 
   return (
     <section className="md:mt-[80px]" id="our-certificates">
-      <TitleAnimation title={t("home.certificates.title")} />
+      <TitleAnimation title={title || ""} />
       <div>
         <CertificateSwiper data={data}/>
       </div>
