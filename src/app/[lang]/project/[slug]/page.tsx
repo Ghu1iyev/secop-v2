14d9a89d977958be3b0d768900da49e8b557f55e
Fazil <guliyevfazil5@gmail.com>
2025-06-18 10:18:42 +0400

    deploy bug fix

--- src/app/[lang]/project/[slug]/page.tsx
+++ src/app/[lang]/project/[slug]/page.tsx
@@ -2,18 +2,16 @@ import { GetApi } from "@/lib/axios";
 import ClientProjectPage from "./clientProjectPage";
 import { DetailProjectProps } from "@/types/common";
 
+
 type PageProps = {
-  params: {
-    lang: string;
+  params: Promise<{
     slug: string;
-  };
+    lang?: string;
+  }>;
 };
 export default async function ProjectPage({ params }: PageProps) {
-  const { slug, lang } = params;
-
-  const language = lang === "en" ? "en" : "az";
-
-  const data = await GetApi(`/projects-detail/${slug}/?lang=${language}`);
-
-  return <ClientProjectPage data={data as DetailProjectProps} />;
-}
+   const { slug, lang } = await params;
+    const language = lang || "az";
+    const data = await GetApi(`/projects-detail/${slug}/?lang=${language}`);
+   return <ClientProjectPage data={data as DetailProjectProps}/>
+} 
